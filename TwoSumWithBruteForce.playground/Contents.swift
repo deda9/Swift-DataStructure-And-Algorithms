83a2ade0e8af9bd82e00c749d5c8339c6de77516
deda9 <Bassem.Qoulta@gmail.com>
2018-10-26 13:51:21 +0200

    Add 2 Sum problem with Brute Force

---
+++ TwoSumWithBruteForce.playground/Contents.swift
@@ -0,0 +1,22 @@
+
+/**
+ * two sum algorithm, take an array and tagert and return the indices which thier sum == this target
+ */
+class TwoSum {
+    static func excute(in array: [Int], target: Int) -> (Int, Int){
+        var indices = (-1, -1)
+        for i in 0..<array.count {
+            for j in 0..<array.count where j != i {
+                let sum = array[i] + array[j]
+                if sum == target {
+                    indices = (j, i)
+                }
+            }
+        }
+        return indices
+    }
+}
+
+let array = [1, 2, 3, 5, 10]
+let indices = TwoSum.excute(in: array, target: 11)
+print(indices)
